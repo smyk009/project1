c600bc0f97c470290898c3c8f8c7c6a42aa38ad5
Samyak <smyk009@gmail.com>
2018-09-17 16:17:18 +0530

    PR1 : Initial Commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,33 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : 'app-server', 
+    templateUrl : './server.component.html'
+})
+export class ServerComponent{
+    serverName : string = 'Server1';
+    serverId : number = 1;
+    serverStatus : String = 'offline';
+    allowAddServer : boolean = false;
+    createServerStatus : String = 'Server Existing';
+    inputName = '';
+
+    constructor(){
+        setTimeout(() => {
+            this.allowAddServer = true;
+        }, 2000);
+    }
+
+    getServerStatus(){
+        return this.serverStatus;
+    }
+
+    onAddServer(){
+        this.createServerStatus = 'New Server Added';
+    }
+
+    onInputServer(event : any){
+        this.inputName = (<HTMLInputElement>event.target).value;
+    }
+
+}
